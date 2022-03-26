b798c1171fc79e45fba9d19949c5e9517eb6ed89
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2022-03-26 20:45:11 +0000

    updating data.R

--- R/data.R
+++ R/data.R
@@ -5,14 +5,14 @@
 #'
 #' @format A data frame of class "tbl" with 339 rows and 4 variables:
 #' \describe{
-#'   \item{lad_19_name}{LAD name}
-#'   \item{lad_19_code}{LAD code}
-#'   \item{county_ua_19_name}{County name}
-#'   \item{county_ua_19_code}{County code}
+#'   \item{ltla19_name}{LAD name}
+#'   \item{ltla19_code}{LAD code}
+#'   \item{utla19_name}{County name}
+#'   \item{utla19_code}{County code}
 #'   ...
 #' }
 #' @source \url{https://geoportal.statistics.gov.uk/}
-"lookup_lad_19_counties_ua_19"
+"lookup_ltla19_utla19"
 
 #' Local Authority Districts to County and Unitary Authorities lookup (2020).
 #'
@@ -21,14 +21,14 @@
 #'
 #' @format A data frame of class "tbl" with 336 rows and 4 variables:
 #' \describe{
-#'   \item{lad_20_name}{LAD name}
-#'   \item{lad_20_code}{LAD code}
-#'   \item{county_ua_20_name}{County name}
-#'   \item{county_ua_20_code}{County code}
+#'   \item{ltla20_name}{LAD name}
+#'   \item{ltla20_code}{LAD code}
+#'   \item{utla20_name}{County name}
+#'   \item{utla20_code}{County code}
 #'   ...
 #' }
 #' @source \url{https://geoportal.statistics.gov.uk/}
-"lookup_lad_20_counties_ua_20"
+"lookup_ltla20_utla20"
 
 #' Local Authority Districts to County and Unitary Authorities lookup (2021).
 #'
@@ -37,78 +37,29 @@
 #'
 #' @format A data frame of class "tbl" with 331 rows and 4 variables:
 #' \describe{
-#'   \item{lad_21_name}{LAD name}
-#'   \item{lad_21_code}{LAD code}
-#'   \item{county_ua_21_name}{County name}
-#'   \item{county_ua_21_code}{County code}
+#'   \item{ltla21_name}{LAD name}
+#'   \item{ltla21_code}{LAD code}
+#'   \item{utla21_name}{County name}
+#'   \item{utla21_code}{County code}
 #'   ...
 #' }
 #' @source \url{https://geoportal.statistics.gov.uk/}
-"lookup_lad_21_counties_ua_21"
+"lookup_ltla21_utla21"
 
-#' Population estimates for Counties and Unitary Authorities (mid-2019 values with 2019 codes)
+#' Population estimates for LSOAs in England and Wales (mid-2019 estimates with 2011 codes)
 #'
-#' A data set containing mid-2019 population estimates for County and Unitary
-#' Authority Districts (April 2019 codes) in each devolved nation.
+#' A data set containing mid-2019 population estimates for Lower Layer Super Output Areas in England and Wales.
 #'
-#' @format A data frame of class "tbl" with 173 rows and 94 variables:
+#' @format A data frame of class "tbl" with 34,753 rows and 94 variables:
 #' \describe{
-#'   \item{county_ua_19_name}{County or Unitary Authority name}
-#'   \item{county_ua_19_code}{County or Unitary Authority code}
-#'   \item{total_population}{Total number of people living in each area}
+#'   \item{lsoa11_name}{LSOA name}
+#'   \item{lsoa11_code}{LSOA code}
+#'   \item{total_population}{Total number of people living in each LSOA}
 #'   \item{`0`:`90+`}{Number of people in each single age group}
 #'   ...
 #' }
 #' @source \url{https://ons.gov.uk/}
-"population_counties_ua_19_codes_19"
-
-
-#' Population estimates for Counties and Unitary Authorities (mid-2019 values with 2020 codes)
-#'
-#' A data set containing mid-2019 population estimates for County and Unitary
-#' Authority Districts (April 2020 codes) in each devolved nation.
-#'
-#' @format A data frame of class "tbl" with 173 rows and 94 variables:
-#' \describe{
-#'   \item{county_ua_20_name}{County or Unitary Authority name}
-#'   \item{county_ua_20_code}{County or Unitary Authority code}
-#'   \item{total_population}{Total number of people living in each area}
-#'   \item{`0`:`90+`}{Number of people in each single age group}
-#'   ...
-#' }
-#' @source \url{https://ons.gov.uk/}
-"population_counties_ua_19_codes_20"
-
-#' Population estimates for Counties and Unitary Authorities (mid-2020 values with 2020 codes)
-#'
-#' A data set containing mid-2020 population estimates for County and Unitary
-#' Authority Districts (April 2020 codes) in each devolved nation.
-#'
-#' @format A data frame of class "tbl" with 172 rows and 94 variables:
-#' \describe{
-#'   \item{county_ua_20_name}{County or Unitary Authority name}
-#'   \item{county_ua_20_code}{County or Unitary Authority code}
-#'   \item{total_population}{Total number of people living in each area}
-#'   \item{`0`:`90+`}{Number of people in each single age group}
-#'   ...
-#' }
-#' @source \url{https://ons.gov.uk/}
-"population_counties_ua_20_codes_20"
-
-#' Population estimates for Data Zones in Scotland (2020 estimates)
-#'
-#' A data set containing 2020 population estimates by age and sex for Data Zones in Scotland.
-#'
-#' @format A data frame of class "tbl" with 20,928 rows and 94 variables:
-#' \describe{
-#'   \item{dz_code}{Data Zone code}
-#'   \item{total_population}{Total number of people living in each DZ}
-#'   \item{sex}{'Female', 'Male' or 'All'}
-#'   \item{`0`:`90+`}{Number of people in each single age group}
-#'   ...
-#' }
-#' @source \url{https://www.opendata.nhs.scot/}
-"population_dz_20"
+"population19_lsoa11"
 
 #' Population estimates for Local Authority Districts (mid-2019 values with 2019 codes)
 #'
@@ -117,15 +68,14 @@
 #'
 #' @format A data frame of class "tbl" with 339 rows and 94 variables:
 #' \describe{
-#'   \item{lad_19_name}{LAD name}
-#'   \item{lad_19_code}{LAD code}
+#'   \item{ltla19_name}{LAD name}
+#'   \item{ltla19_code}{LAD code}
 #'   \item{total_population}{Total number of people living in each LAD}
 #'   \item{`0`:`90+`}{Number of people in each single age group}
 #'   ...
 #' }
 #' @source \url{https://ons.gov.uk/}
-"population_lad_19_codes_19"
-
+"population19_ltla19"
 
 #' Population estimates for Local Authority Districts (mid-2019 values with 2020 codes)
 #'
@@ -134,14 +84,92 @@
 #'
 #' @format A data frame of class "tbl" with 336 rows and 94 variables:
 #' \describe{
-#'   \item{lad_20_name}{LAD name}
-#'   \item{lad_20_code}{LAD code}
+#'   \item{ltla20_name}{LAD name}
+#'   \item{ltla20_code}{LAD code}
 #'   \item{total_population}{Total number of people living in each LAD}
 #'   \item{`0`:`90+`}{Number of people in each single age group}
 #'   ...
 #' }
 #' @source \url{https://ons.gov.uk/}
-"population_lad_19_codes_20"
+"population19_ltla20"
+
+#' Population estimates for MSOAs in England and Wales (mid-2019 estimates with 2011 codes)
+#'
+#' A data set containing mid-2019 population estimates for Middle Layer Super Output Areas in England and Wales.
+#'
+#' @format A data frame of class "tbl" with 7,201 rows and 94 variables:
+#' \describe{
+#'   \item{msoa11_name}{MSOA name}
+#'   \item{msoa11_code}{MSOA code}
+#'   \item{total_population}{Total number of people living in each MSOA}
+#'   \item{`0`:`90+`}{Number of people in each single age group}
+#'   ...
+#' }
+#' @source \url{https://ons.gov.uk/}
+"population19_msoa11"
+
+#' Population estimates for Counties and Unitary Authorities (mid-2019 values with 2019 codes)
+#'
+#' A data set containing mid-2019 population estimates for County and Unitary
+#' Authority Districts (April 2019 codes) in each devolved nation.
+#'
+#' @format A data frame of class "tbl" with 173 rows and 94 variables:
+#' \describe{
+#'   \item{utla19_name}{County or Unitary Authority name}
+#'   \item{utla19_code}{County or Unitary Authority code}
+#'   \item{total_population}{Total number of people living in each area}
+#'   \item{`0`:`90+`}{Number of people in each single age group}
+#'   ...
+#' }
+#' @source \url{https://ons.gov.uk/}
+"population19_utla19"
+
+#' Population estimates for Counties and Unitary Authorities (mid-2019 values with 2020 codes)
+#'
+#' A data set containing mid-2019 population estimates for County and Unitary
+#' Authority Districts (April 2020 codes) in each devolved nation.
+#'
+#' @format A data frame of class "tbl" with 173 rows and 94 variables:
+#' \describe{
+#'   \item{utla20_name}{County or Unitary Authority name}
+#'   \item{utla20_code}{County or Unitary Authority code}
+#'   \item{total_population}{Total number of people living in each area}
+#'   \item{`0`:`90+`}{Number of people in each single age group}
+#'   ...
+#' }
+#' @source \url{https://ons.gov.uk/}
+"population19_utla20"
+
+
+#' Population estimates for Data Zones in Scotland (2020 estimates with 2011 codes)
+#'
+#' A data set containing 2020 population estimates by age and sex for Data Zones in Scotland.
+#'
+#' @format A data frame of class "tbl" with 20,928 rows and 94 variables:
+#' \describe{
+#'   \item{dz11_code}{Data Zone code}
+#'   \item{total_population}{Total number of people living in each DZ}
+#'   \item{sex}{'Female', 'Male' or 'All'}
+#'   \item{`0`:`90+`}{Number of people in each single age group}
+#'   ...
+#' }
+#' @source \url{https://www.opendata.nhs.scot/}
+"population20_dz11"
+
+#' Population estimates for LSOAs in England and Wales (mid-2020 estimates with 2011 codes)
+#'
+#' A data set containing mid-2020 population estimates for Lower Layer Super Output Areas in England and Wales.
+#'
+#' @format A data frame of class "tbl" with 34,753 rows and 94 variables:
+#' \describe{
+#'   \item{lsoa11_name}{LSOA name}
+#'   \item{lsoa11_code}{LSOA code}
+#'   \item{total_population}{Total number of people living in each LSOA}
+#'   \item{`0`:`90+`}{Number of people in each single age group}
+#'   ...
+#' }
+#' @source \url{https://ons.gov.uk/}
+"population20_lsoa11"
 
 #' Population estimates for Local Authority Districts (mid-2020 values with 2020 codes)
 #'
@@ -150,14 +178,14 @@
 #'
 #' @format A data frame of class "tbl" with 336 rows and 94 variables:
 #' \describe{
-#'   \item{lad_20_name}{LAD name}
-#'   \item{lad_20_code}{LAD code}
+#'   \item{ltla20_name}{LAD name}
+#'   \item{ltla20_code}{LAD code}
 #'   \item{total_population}{Total number of people living in each LAD}
 #'   \item{`0`:`90+`}{Number of people in each single age group}
 #'   ...
 #' }
 #' @source \url{https://ons.gov.uk/}
-"population_lad_20_codes_20"
+"population20_ltla20"
 
 #' Population estimates for Local Authority Districts (mid-2020 values with 2021 codes)
 #'
@@ -166,59 +194,14 @@
 #'
 #' @format A data frame of class "tbl" with 331 rows and 94 variables:
 #' \describe{
-#'   \item{lad_21_name}{LAD name}
-#'   \item{lad_21_code}{LAD code}
+#'   \item{ltla21_name}{LAD name}
+#'   \item{ltla21_code}{LAD code}
 #'   \item{total_population}{Total number of people living in each LAD}
 #'   \item{`0`:`90+`}{Number of people in each single age group}
 #'   ...
 #' }
 #' @source \url{https://ons.gov.uk/}
-"population_lad_20_codes_21"
-
-#' Population estimates for LSOAs in England and Wales (mid-2019 estimates with 2011 codes)
-#'
-#' A data set containing mid-2019 population estimates for Lower Layer Super Output Areas in England and Wales.
-#'
-#' @format A data frame of class "tbl" with 34,753 rows and 94 variables:
-#' \describe{
-#'   \item{lsoa_11_name}{LSOA name}
-#'   \item{lsoa_11_code}{LSOA code}
-#'   \item{total_population}{Total number of people living in each LSOA}
-#'   \item{`0`:`90+`}{Number of people in each single age group}
-#'   ...
-#' }
-#' @source \url{https://ons.gov.uk/}
-"population_lsoa_19_codes_11"
-
-#' Population estimates for LSOAs in England and Wales (mid-2020 estimates with 2011 codes)
-#'
-#' A data set containing mid-2020 population estimates for Lower Layer Super Output Areas in England and Wales.
-#'
-#' @format A data frame of class "tbl" with 34,753 rows and 94 variables:
-#' \describe{
-#'   \item{lsoa_11_name}{LSOA name}
-#'   \item{lsoa_11_code}{LSOA code}
-#'   \item{total_population}{Total number of people living in each LSOA}
-#'   \item{`0`:`90+`}{Number of people in each single age group}
-#'   ...
-#' }
-#' @source \url{https://ons.gov.uk/}
-"population_lsoa_20_codes_11"
-
-#' Population estimates for MSOAs in England and Wales (mid-2019 estimates with 2011 codes)
-#'
-#' A data set containing mid-2019 population estimates for Middle Layer Super Output Areas in England and Wales.
-#'
-#' @format A data frame of class "tbl" with 7,201 rows and 94 variables:
-#' \describe{
-#'   \item{msoa_11_name}{MSOA name}
-#'   \item{msoa_11_code}{MSOA code}
-#'   \item{total_population}{Total number of people living in each MSOA}
-#'   \item{`0`:`90+`}{Number of people in each single age group}
-#'   ...
-#' }
-#' @source \url{https://ons.gov.uk/}
-"population_msoa_19_codes_11"
+"population20_ltla21"
 
 #' Population estimates for MSOAs in England and Wales (mid-2020 estimates with 2011 codes)
 #'
@@ -226,12 +209,27 @@
 #'
 #' @format A data frame of class "tbl" with 7,201 rows and 94 variables:
 #' \describe{
-#'   \item{msoa_11_name}{MSOA name}
-#'   \item{msoa_11_code}{MSOA code}
+#'   \item{msoa11_name}{MSOA name}
+#'   \item{msoa11_code}{MSOA code}
 #'   \item{total_population}{Total number of people living in each MSOA}
 #'   \item{`0`:`90+`}{Number of people in each single age group}
 #'   ...
 #' }
 #' @source \url{https://ons.gov.uk/}
-"population_msoa_20_codes_11"
+"population20_msoa11"
 
+#' Population estimates for Counties and Unitary Authorities (mid-2020 values with 2020 codes)
+#'
+#' A data set containing mid-2020 population estimates for County and Unitary
+#' Authority Districts (April 2020 codes) in each devolved nation.
+#'
+#' @format A data frame of class "tbl" with 172 rows and 94 variables:
+#' \describe{
+#'   \item{utla20_name}{County or Unitary Authority name}
+#'   \item{utla20_code}{County or Unitary Authority code}
+#'   \item{total_population}{Total number of people living in each area}
+#'   \item{`0`:`90+`}{Number of people in each single age group}
+#'   ...
+#' }
+#' @source \url{https://ons.gov.uk/}
+"population20_utla20"
