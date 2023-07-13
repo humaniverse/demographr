8e46ec4dffde22e55b7b25ac594b7cb3573275ab
MatteoLarrode <matteo.larrode@orange.fr>
2023-07-13 10:49:58 +0100

    created data-raw/ file for ethnicity in ni at ltla level

---
+++ data-raw/ethnicity21_ltla21_ni.R
@@ -0,0 +1,27 @@
+# Ethnicity demographic indicators in Northern Ireland local authorities
+
+# ---- Load libs ----
+library(tidyverse)
+library(readxl)
+library(devtools)
+library(httr2)
+
+# ---- Load internal sysdata.rda file with URL's ----
+load_all(".")
+
+# ---- Download data ----
+query_url <-
+  query_urls |>
+  filter(id == "age_gender_ltla21_ni") |>
+  pull(query)
+
+download <- tempfile(fileext = ".xlsx")
+
+request(query_url) |>
+  req_perform(download)
+
+raw <- read_excel(
+  download,
+  sheet = "MS-B01",
+  range = "A9:BJ21"
+)
